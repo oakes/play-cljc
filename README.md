31680f6eb2e133c78277ae40f30bb35a888b481b
oakes <zach@sekao.net>
2019-02-24 20:20:52 -0600

    Start messing with lwjgl

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A Clojure and ClojureScript library for making games that run in both OpenGL and
 ## Development
 
 * Install [the Clojure CLI tool](https://clojure.org/guides/getting_started#_clojure_installer_and_cli_tools)
-* To develop with figwheel: `clj -A:dev dev.clj`
+* To develop the web version with figwheel: `clj -A:dev dev.clj`
+* To develop the native version: `clj -A:dev -J-XstartOnFirstThread dev.clj native`
 * To install the release version: `clj -A:prod prod.clj install`
 
 ## Licensing
--- dev.clj
+++ dev.clj
@@ -1,7 +1,47 @@
+(defmulti task first)
+
+(defmethod task :default
+  [_]
+  (let [all-tasks  (-> task methods (dissoc :default) keys sort)
+        interposed (->> all-tasks (interpose ", ") (apply str))]
+    (println "Unknown or missing task. Choose one of:" interposed)
+    (System/exit 1)))
+
 (require
   '[figwheel.main :as figwheel]
   '[dynadoc.core :as dynadoc])
 
-(dynadoc/-main "--port" "5000")
-(figwheel/-main "--build" "dev")
+(defmethod task nil
+  [_]
+  (dynadoc/-main "--port" "5000")
+  (figwheel/-main "--build" "dev"))
 
+(import '[org.lwjgl.glfw GLFW Callbacks]
+        '[org.lwjgl.opengl GL GL30])
+
+(defmethod task "native"
+  [_]
+  (when-not (GLFW/glfwInit)
+    (throw (Exception. "Unable to initialize GLFW")))
+  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
+  (if-let [window (GLFW/glfwCreateWindow 300 300 "Hello world!" 0 0)]
+    (do
+      ;; init
+      (GLFW/glfwMakeContextCurrent window)
+      (GLFW/glfwSwapInterval 1)
+      (GLFW/glfwShowWindow window)
+      ;; loop
+      (GL/createCapabilities)
+      (GL30/glClearColor (float 1) (float 1) (float 1) (float 1))
+      (while (not (GLFW/glfwWindowShouldClose window))
+        (GL30/glClear (bit-or GL30/GL_COLOR_BUFFER_BIT GL30/GL_DEPTH_BUFFER_BIT))
+        (GLFW/glfwSwapBuffers window)
+        (GLFW/glfwPollEvents))
+      ;; clean up
+      (Callbacks/glfwFreeCallbacks window)
+      (GLFW/glfwDestroyWindow window)
+      (GLFW/glfwTerminate))
+    (throw (Exception. "Failed to create window"))))
+
+(task *command-line-args*)
