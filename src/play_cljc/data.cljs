4cc97b9114e9fd20d53520962969c9d03a8af80e
oakes <zach@sekao.net>
2019-02-26 18:46:04 -0600

    Move shader data into separate vars

--- src/play_cljc/data.cljs
+++ src/play_cljc/data.cljs
@@ -1,221 +1,241 @@
 (ns play-cljc.data
   (:require [iglu.core :as ig]))
 
+(def image-vertex-shader
+  '{:type :vertex
+    :version "300 es"
+    :attributes
+    {a_position vec2}
+    :uniforms
+    {u_matrix mat3
+     u_image sampler2D}
+    :varyings
+    {v_texCoord vec2}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([]
+           [:= gl_Position
+            [:vec4
+             [:-xy [:* u_matrix [:vec3 a_position 1]]]
+             0 1]]
+           [:= v_texCoord a_position])}})
+
 (def image-vertex-shader-source
-  (ig/iglu->glsl
-    '{:type :vertex
-      :version "300 es"
-      :attributes
-      {a_position vec2}
-      :uniforms
-      {u_matrix mat3
-       u_image sampler2D}
-      :varyings
-      {v_texCoord vec2}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([]
-             [:= gl_Position
-              [:vec4
-               [:-xy [:* u_matrix [:vec3 a_position 1]]]
-               0 1]]
-             [:= v_texCoord a_position])}}))
+  (ig/iglu->glsl image-vertex-shader))
+
+(def image-fragment-shader
+  '{:type :fragment
+    :version "300 es"
+    :precision "mediump float"
+    :uniforms
+    {u_image sampler2D}
+    :varyings
+    {v_texCoord vec2}
+    :outputs
+    {outColor vec4}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([] [:= outColor [:-bgra [:texture u_image v_texCoord]]])}})
 
 (def image-fragment-shader-source
-  (ig/iglu->glsl
-    '{:type :fragment
-      :version "300 es"
-      :precision "mediump float"
-      :uniforms
-      {u_image sampler2D}
-      :varyings
-      {v_texCoord vec2}
-      :outputs
-      {outColor vec4}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([] [:= outColor [:-bgra [:texture u_image v_texCoord]]])}}))
+  (ig/iglu->glsl image-fragment-shader))
+
+(def two-d-vertex-shader
+  '{:type :vertex
+    :version "300 es"
+    :attributes
+    {a_position vec2}
+    :uniforms
+    {u_matrix mat3}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([]
+           [:= gl_Position
+            [:vec4
+             [:-xy [:* u_matrix [:vec3 a_position 1]]]
+             0 1]])}})
 
 (def two-d-vertex-shader-source
-  (ig/iglu->glsl
-    '{:type :vertex
-      :version "300 es"
-      :attributes
-      {a_position vec2}
-      :uniforms
-      {u_matrix mat3}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([]
-             [:= gl_Position
-              [:vec4
-               [:-xy [:* u_matrix [:vec3 a_position 1]]]
-               0 1]])}}))
+  (ig/iglu->glsl two-d-vertex-shader))
+
+(def two-d-fragment-shader
+  '{:type :fragment
+    :version "300 es"
+    :precision "mediump float"
+    :uniforms
+    {u_color vec4}
+    :outputs
+    {outColor vec4}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([] [:= outColor u_color])}})
 
 (def two-d-fragment-shader-source
-  (ig/iglu->glsl
-    '{:type :fragment
-      :version "300 es"
-      :precision "mediump float"
-      :uniforms
-      {u_color vec4}
-      :outputs
-      {outColor vec4}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([] [:= outColor u_color])}}))
+  (ig/iglu->glsl two-d-fragment-shader))
+
+(def three-d-vertex-shader
+  '{:type :vertex
+    :version "300 es"
+    :attributes
+    {a_position vec4
+     a_color vec4}
+    :uniforms
+    {u_matrix mat4}
+    :varyings
+    {v_color vec4}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([]
+           [:= gl_Position [:* u_matrix a_position]]
+           [:= v_color a_color])}})
 
 (def three-d-vertex-shader-source
-  (ig/iglu->glsl
-    '{:type :vertex
-      :version "300 es"
-      :attributes
-      {a_position vec4
-       a_color vec4}
-      :uniforms
-      {u_matrix mat4}
-      :varyings
-      {v_color vec4}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([]
-             [:= gl_Position [:* u_matrix a_position]]
-             [:= v_color a_color])}}))
+  (ig/iglu->glsl three-d-vertex-shader))
+
+(def three-d-fragment-shader
+  '{:type :fragment
+    :version "300 es"
+    :precision "mediump float"
+    :varyings
+    {v_color vec4}
+    :outputs
+    {outColor vec4}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([] [:= outColor v_color])}})
 
 (def three-d-fragment-shader-source
-  (ig/iglu->glsl
-    '{:type :fragment
-      :version "300 es"
-      :precision "mediump float"
-      :varyings
-      {v_color vec4}
-      :outputs
-      {outColor vec4}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([] [:= outColor v_color])}}))
+  (ig/iglu->glsl three-d-fragment-shader))
+
+(def texture-vertex-shader
+  '{:type :vertex
+    :version "300 es"
+    :attributes
+    {a_position vec4
+     a_color vec4
+     a_texcoord vec2}
+    :uniforms
+    {u_matrix mat4
+     u_texture sampler2D}
+    :varyings
+    {v_texcoord vec2}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([]
+           [:= gl_Position [:* u_matrix a_position]]
+           [:= v_texcoord a_texcoord])}})
 
 (def texture-vertex-shader-source
-  (ig/iglu->glsl
-    '{:type :vertex
-      :version "300 es"
-      :attributes
-      {a_position vec4
-       a_color vec4
-       a_texcoord vec2}
-      :uniforms
-      {u_matrix mat4
-       u_texture sampler2D}
-      :varyings
-      {v_texcoord vec2}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([]
-             [:= gl_Position [:* u_matrix a_position]]
-             [:= v_texcoord a_texcoord])}}))
+  (ig/iglu->glsl texture-vertex-shader))
+
+(def texture-fragment-shader
+  '{:type :fragment
+    :version "300 es"
+    :precision "mediump float"
+    :uniforms
+    {u_texture sampler2D}
+    :varyings
+    {v_texcoord vec2}
+    :outputs
+    {outColor vec4}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([] [:= outColor [:texture u_texture v_texcoord]])}})
 
 (def texture-fragment-shader-source
-  (ig/iglu->glsl
-    '{:type :fragment
-      :version "300 es"
-      :precision "mediump float"
-      :uniforms
-      {u_texture sampler2D}
-      :varyings
-      {v_texcoord vec2}
-      :outputs
-      {outColor vec4}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([] [:= outColor [:texture u_texture v_texcoord]])}}))
+  (ig/iglu->glsl texture-fragment-shader))
+
+(def advanced-vertex-shader
+  '{:type :vertex
+    :version "300 es"
+    :uniforms
+    {u_worldViewProjection mat4
+     u_lightWorldPos vec3
+     u_world mat4
+     u_viewInverse mat4
+     u_worldInverseTranspose mat4}
+    :attributes
+    {a_position vec4
+     a_normal vec3
+     a_texCoord vec2}
+    :varyings
+    {v_position vec4
+     v_texCoord vec2
+     v_normal vec3
+     v_surfaceToLight vec3
+     v_surfaceToView vec3}
+    :signatures
+    {main ([] void)}
+    :functions
+    {main ([]
+           [:= v_texCoord a_texCoord]
+           [:= v_position [:* u_worldViewProjection a_position]]
+           [:= v_normal [:-xyz [:* u_worldInverseTranspose [:vec4 a_normal 0]]]]
+           [:= v_surfaceToLight [:- u_lightWorldPos [:-xyz [:* u_world a_position]]]]
+           [:= v_surfaceToView [:-xyz [:- [3 u_viewInverse] [:* u_world a_position]]]]
+           [:= gl_Position v_position])}})
 
 (def advanced-vertex-shader-source
-  (ig/iglu->glsl
-    '{:type :vertex
-      :version "300 es"
-      :uniforms
-      {u_worldViewProjection mat4
-       u_lightWorldPos vec3
-       u_world mat4
-       u_viewInverse mat4
-       u_worldInverseTranspose mat4}
-      :attributes
-      {a_position vec4
-       a_normal vec3
-       a_texCoord vec2}
-      :varyings
-      {v_position vec4
-       v_texCoord vec2
-       v_normal vec3
-       v_surfaceToLight vec3
-       v_surfaceToView vec3}
-      :signatures
-      {main ([] void)}
-      :functions
-      {main ([]
-             [:= v_texCoord a_texCoord]
-             [:= v_position [:* u_worldViewProjection a_position]]
-             [:= v_normal [:-xyz [:* u_worldInverseTranspose [:vec4 a_normal 0]]]]
-             [:= v_surfaceToLight [:- u_lightWorldPos [:-xyz [:* u_world a_position]]]]
-             [:= v_surfaceToView [:-xyz [:- [3 u_viewInverse] [:* u_world a_position]]]]
-             [:= gl_Position v_position])}}))
+  (ig/iglu->glsl advanced-vertex-shader))
+
+(def advanced-fragment-shader
+  '{:type :fragment
+    :version "300 es"
+    :precision "mediump float"
+    :uniforms
+    {u_lightColor vec4
+     u_color vec4
+     u_specular vec4
+     u_shininess float
+     u_specularFactor float}
+    :varyings
+    {v_position vec4
+     v_texCoord vec2
+     v_normal vec3
+     v_surfaceToLight vec3
+     v_surfaceToView vec3}
+    :outputs
+    {outColor vec4}
+    :signatures
+    {lit ([float float float] vec4)
+     main ([] void)}
+    :functions
+    {lit ([l h m]
+          [:vec4
+           "1.0"
+           [:abs l]
+           [:? [:> l "0.0"]
+            [:pow [:max "0.0" h] m]
+            "0.0"]
+           "1.0"])
+     main ([]
+           [:=vec3 a_normal [:normalize v_normal]]
+           [:=vec3 surfaceToLight [:normalize v_surfaceToLight]]
+           [:=vec3 surfaceToView [:normalize v_surfaceToView]]
+           [:=vec3 halfVector [:normalize [:+ surfaceToLight surfaceToView]]]
+           [:=vec4 litR [lit
+                         [:dot a_normal surfaceToLight]
+                         [:dot a_normal halfVector]
+                         u_shininess]]
+           [:= outColor
+            [:vec4
+             [:-rgb
+              [:* u_lightColor
+               [:+
+                [:* [:-y litR] u_color]
+                [:* u_specular [:-z litR] u_specularFactor]]]]
+             1]])}})
 
 (def advanced-fragment-shader-source
-  (ig/iglu->glsl
-    '{:type :fragment
-      :version "300 es"
-      :precision "mediump float"
-      :uniforms
-      {u_lightColor vec4
-       u_color vec4
-       u_specular vec4
-       u_shininess float
-       u_specularFactor float}
-      :varyings
-      {v_position vec4
-       v_texCoord vec2
-       v_normal vec3
-       v_surfaceToLight vec3
-       v_surfaceToView vec3}
-      :outputs
-      {outColor vec4}
-      :signatures
-      {lit ([float float float] vec4)
-       main ([] void)}
-      :functions
-      {lit ([l h m]
-            [:vec4
-             "1.0"
-             [:abs l]
-             [:? [:> l "0.0"]
-              [:pow [:max "0.0" h] m]
-              "0.0"]
-             "1.0"])
-       main ([]
-             [:=vec3 a_normal [:normalize v_normal]]
-             [:=vec3 surfaceToLight [:normalize v_surfaceToLight]]
-             [:=vec3 surfaceToView [:normalize v_surfaceToView]]
-             [:=vec3 halfVector [:normalize [:+ surfaceToLight surfaceToView]]]
-             [:=vec4 litR [lit
-                           [:dot a_normal surfaceToLight]
-                           [:dot a_normal halfVector]
-                           u_shininess]]
-             [:= outColor
-              [:vec4
-               [:-rgb
-                [:* u_lightColor
-                 [:+
-                  [:* [:-y litR] u_color]
-                  [:* u_specular [:-z litR] u_specularFactor]]]]
-               1]])}}))
+  (ig/iglu->glsl advanced-fragment-shader))
 
 (def rect
   ;; x1 y1, x2 y1, x1 y2, x1 y2, x2 y1, x2 y2
