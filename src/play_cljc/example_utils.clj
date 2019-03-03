3925e98b5e829aea788256875eacbf9a24164db2
oakes <zach@sekao.net>
2019-03-03 15:11:55 -0600

    Create skeleton example-utils ns for clj

---
+++ src/play_cljc/example_utils.clj
@@ -0,0 +1,10 @@
+(ns play-cljc.example-utils)
+
+(defn init-example [card])
+
+(defn resize-example [game])
+
+(defn listen-for-mouse [{:keys [tx ty] :or {tx 0 ty 0}} callback])
+
+(defn get-image [fname callback])
+
