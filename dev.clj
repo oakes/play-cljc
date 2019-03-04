bbaf8be5da0e162e99d1c2d1a9fdc1a351d3492f
oakes <zach@sekao.net>
2019-03-04 14:25:46 -0600

    Draw rectangle

--- dev.clj
+++ dev.clj
@@ -46,6 +46,8 @@
       (let [program (u/create-program nil
                       (ig/iglu->glsl :vertex data/two-d-vertex-shader)
                       (ig/iglu->glsl :fragment data/two-d-fragment-shader))
+            vao (GL41/glGenVertexArrays)
+            _ (GL41/glBindVertexArray vao)
             cnt (u/create-buffer nil program "a_position" (float-array data/rect)
                                  {:type GL41/GL_FLOAT
                                   :size 2
@@ -54,15 +56,18 @@
                                   :offset 0})
             color-location (GL41/glGetUniformLocation program "u_color")
             matrix-location (GL41/glGetUniformLocation program "u_matrix")]
+        (GL41/glUseProgram program)
+        (GL41/glViewport 0 0 300 300)
         (GL41/glUniform4fv color-location (float-array [(rand) (rand) (rand) 1]))
         (GL41/glUniformMatrix3fv matrix-location false
                                  (->> (m/projection-matrix 300 300)
-                                      (m/multiply-matrices 3 (m/translation-matrix (rand-int 300) (rand-int 300)))
-                                      (m/multiply-matrices 3 (m/scaling-matrix (rand-int 300) (rand-int 300)))
+                                      (m/multiply-matrices 3 (m/translation-matrix 0 0))
+                                      (m/multiply-matrices 3 (m/scaling-matrix 100 100))
                                       float-array))
         (GL41/glClearColor (float 1) (float 1) (float 1) (float 1))
         (while (not (GLFW/glfwWindowShouldClose window))
           (GL41/glClear (bit-or GL41/GL_COLOR_BUFFER_BIT GL41/GL_DEPTH_BUFFER_BIT))
+          (GL41/glDrawArrays GL41/GL_TRIANGLES 0 cnt)
           (GLFW/glfwSwapBuffers window)
           (GLFW/glfwPollEvents)))
       ;; clean up
