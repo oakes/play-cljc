6014ae8859631e287cd9d88c54b5a3926fb2df30
oakes <zach@sekao.net>
2019-03-04 12:32:13 -0600

    Clean up macros

--- src/play_cljc/macros_java.clj
+++ src/play_cljc/macros_java.clj
@@ -1,20 +1,18 @@
 (ns play-cljc.macros-java)
 
 (defmacro math [n & args]
-  (let [s (str n)]
-    (if-let [l (first s)]
-      (if (Character/isUpperCase l)
-        (symbol (str 'Math "/" s))
-        (cons (symbol (str 'Math "/" s)) args))
-      (throw (Exception. "Invalid method or property name")))))
+  (let [s (str n)
+        l (nth s 0)]
+    (if (Character/isUpperCase l)
+      (symbol (str 'Math "/" s))
+      (cons (symbol (str 'Math "/" s)) args))))
 
 (defmacro gl [_ n & args]
   (let [s (str n)
+        l (nth s 0)
         remaining-letters (subs s 1)]
-    (if-let [l (first s)]
-      (if (Character/isUpperCase l)
-        (symbol (str "org.lwjgl.opengl.GL41/GL_" s))
-        (cons (symbol (str "org.lwjgl.opengl.GL41/gl" (Character/toUpperCase l) remaining-letters))
-              args))
-      (throw (Exception. "Invalid method or property name")))))
+    (if (Character/isUpperCase l)
+      (symbol (str "org.lwjgl.opengl.GL41/GL_" s))
+      (cons (symbol (str "org.lwjgl.opengl.GL41/gl" (Character/toUpperCase l) remaining-letters))
+            args))))
 
--- src/play_cljc/macros_js.clj
+++ src/play_cljc/macros_js.clj
@@ -1,19 +1,16 @@
 (ns play-cljc.macros-js)
 
 (defmacro math [n & args]
-  (let [s (str n)]
-    (if-let [l (first s)]
-      (if (Character/isUpperCase l)
-        (symbol (str 'js "/Math." s))
-        (cons (symbol (str 'js "/Math." s)) args))
-      (throw (Exception. "Invalid method or property name")))))
+  (let [s (str n)
+        l (nth s 0)]
+    (if (Character/isUpperCase l)
+      (symbol (str 'js "/Math." s))
+      (cons (symbol (str 'js "/Math." s)) args))))
 
 (defmacro gl [game n & args]
   (let [s (str n)
-        remaining-letters (subs s 1)]
-    (if-let [l (first s)]
-      (if (Character/isUpperCase l)
-        `(goog.object/get (:gl ~game) ~s)
-        (concat [(symbol (str "." s)) `(:gl ~game)] args))
-      (throw (Exception. "Invalid method or property name")))))
+        l (nth s 0)]
+    (if (Character/isUpperCase l)
+      `(goog.object/get (:gl ~game) ~s)
+      (concat [(symbol (str "." s)) `(:gl ~game)] args))))
 
