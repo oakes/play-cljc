84dcef95d8ead4a925bdfb3822f56ce30df7746f
oakes <zach@sekao.net>
2019-03-10 18:42:35 -0600

    Start 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc "0.1.0"
+(defproject play-cljc "0.2.0-SNAPSHOT"
   :description "A Clojure and ClojureScript game library"
   :url "https://github.com/oakes/play-cljc"
   :license {:name "Public Domain"
