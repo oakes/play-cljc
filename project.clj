fc5f9dabe776b219eacd32ff0d0325885ced94c7
oakes <zach@sekao.net>
2019-03-21 10:57:42 -0600

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc "0.2.0-SNAPSHOT"
+(defproject play-cljc "0.2.0"
   :description "A Clojure and ClojureScript game library"
   :url "https://github.com/oakes/play-cljc"
   :license {:name "Public Domain"
