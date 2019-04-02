a21ee7e27cd38dde15eeea8cc5e3474fadd96a8c
oakes <zach@sekao.net>
2019-04-02 07:24:35 -0600

    Remove :license

--- template/src/leiningen/new/play_cljc/project.clj
+++ template/src/leiningen/new/play_cljc/project.clj
@@ -1,6 +1,4 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
-  :license {:name "Public Domain"
-            :url "http://unlicense.org/UNLICENSE"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
   :clean-targets ^{:protect false} ["target"]
