71723575ff04e82f70208de6989ef0eed12e56ac
oakes <zach@sekao.net>
2019-04-02 07:24:48 -0600

    Increment template version

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc/lein-template "0.3.0.1"
+(defproject play-cljc/lein-template "0.3.0.2"
   :description "A template for making play-cljc projects"
   :url "https://github.com/oakes/play-cljc"
   :license {:name "Public Domain"
