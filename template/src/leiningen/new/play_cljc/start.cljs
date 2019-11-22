ed9ba8c7643815abeee7e6b783622926878a0386
oakes <zach@sekao.net>
2019-11-22 17:29:01 -0500

    Don't resize in the game loop

--- template/src/leiningen/new/play_cljc/start.cljs
+++ template/src/leiningen/new/play_cljc/start.cljs
@@ -4,16 +4,7 @@
             [goog.events :as events])
   (:require-macros [{{name}}.music :refer [build-for-cljs]]))
 
-(defn resize [{:keys [context] :as game}]
-  (let [display-width context.canvas.clientWidth
-        display-height context.canvas.clientHeight]
-    (when (or (not= context.canvas.width display-width)
-              (not= context.canvas.height display-height))
-      (set! context.canvas.width display-width)
-      (set! context.canvas.height display-height))))
-
 (defn game-loop [game]
-  (resize game)
   (let [game (c/tick game)]
     (js/requestAnimationFrame
       (fn [ts]
@@ -49,6 +40,17 @@
       (when-let [k (keycode->keyword (.-keyCode event))]
         (swap! c/*state update :pressed-keys disj k)))))
 
+(defn resize [context]
+  (let [display-width context.canvas.clientWidth
+        display-height context.canvas.clientHeight]
+    (set! context.canvas.width display-width)
+    (set! context.canvas.height display-height)))
+
+(defn listen-for-resize [context]
+  (events/listen js/window "resize"
+    (fn [event]
+      (resize context))))
+
 ;; start the game
 
 (defonce context
@@ -59,6 +61,8 @@
                             :total-time 0)]
     (listen-for-mouse canvas)
     (listen-for-keys)
+    (resize context)
+    (listen-for-resize context)
     (c/init initial-game)
     (game-loop initial-game)
     context))
