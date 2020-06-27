77a498373a9e81bd482cf37005a0d0e95abfeb7c
oakes <zach@sekao.net>
2020-06-27 14:09:18 -0400

    Template: use figwheel's :clean-outputs option

--- template/src/leiningen/new/play_cljc/dev.clj
+++ template/src/leiningen/new/play_cljc/dev.clj
@@ -5,19 +5,10 @@
   (println "Unknown task:" task-name)
   (System/exit 1))
 
-(require
-  '[figwheel.main :as figwheel]
-  '[clojure.java.io :as io])
-
-(defn delete-children-recursively! [f]
-  (when (.isDirectory f)
-    (doseq [f2 (.listFiles f)]
-      (delete-children-recursively! f2)))
-  (when (.exists f) (io/delete-file f)))
+(require '[figwheel.main :as figwheel])
 
 (defmethod task nil
   [_]
-  (delete-children-recursively! (io/file "resources/public/main.out"))
   (figwheel/-main "--build" "dev"))
 
 (require '[{{name}}.start-dev])
