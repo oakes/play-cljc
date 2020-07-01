463385d658284c8fd8af14e2657dbe82abcf2b34
oakes <zach@sekao.net>
2020-07-01 11:34:52 -0400

    Bump version

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc/lein-template "0.8.9"
+(defproject play-cljc/lein-template "0.9.0"
   :description "A template for making play-cljc projects"
   :url "https://github.com/oakes/play-cljc"
   :license {:name "Public Domain"
