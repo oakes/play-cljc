9fd3e7f9facc81c5b3e21974518c689004920880
oakes <zach@sekao.net>
2020-07-12 11:26:37 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc "0.9.2-SNAPSHOT"
+(defproject play-cljc "0.9.2"
   :description "A Clojure and ClojureScript game library"
   :url "https://github.com/oakes/play-cljc"
   :license {:name "Public Domain"
